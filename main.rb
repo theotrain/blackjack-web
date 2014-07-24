2b8d048d4298a896d5366567840c5b2c1dd86878
theotrain <owen@localhost.pk5001z>
2014-07-24 06:51:05 -0400

    animate dealer

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ get '/' do
   erb :name
 end
 
-post '/name' do #respond to name form submission
+post '/name' do 
   if params[:name].lstrip[0]
     "name is legit"
     session[:name] = params[:name]
@@ -25,7 +25,6 @@ get "/game" do
   if session[:name].nil?
     redirect "/"
   else
-    # if session[:deck]
     if session[:deck].nil?  || session[:player_hand].nil? || session[:dealer_hand].nil?
       initialize_decks
       clear_hands 
@@ -33,8 +32,6 @@ get "/game" do
       deal_card(session[:player_hand])
       deal_card(session[:dealer_hand])
     end
-    #puts session[:player_hand].to_s
-    #binding.pry
     erb :game
   end
 end
@@ -43,14 +40,31 @@ post '/game/player_action' do
   if params[:action] == "Hit"
     deal_card(session[:player_hand])
     session[:show_deal] = true
+    session[:dealer_turn] = false
   else #Stay
     session[:dealer_turn] = true
-    while hand_value(session[:dealer_hand]) < 17
-     deal_card(session[:dealer_hand])
+    if hand_value(session[:dealer_hand]) < 17
+      deal_card(session[:dealer_hand])
+      session[:show_deal_top] = true
+    else
+      session[:show_deal_top] = false
     end
   end
   redirect "/game"
 end
+
+# post '/game/player_action' do
+#   if params[:action] == "Hit"
+#     deal_card(session[:player_hand])
+#     session[:show_deal] = true
+#   else #Stay
+#     session[:dealer_turn] = true
+#     while hand_value(session[:dealer_hand]) < 17
+#      deal_card(session[:dealer_hand])
+#     end
+#   end
+#   redirect "/game"
+# end
 
 post '/game/play_again' do
   initialize_decks
